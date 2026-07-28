26dc8fc89df382f8443fe9114d9523d41aa65c8b
emmanuelkine <emmanuelkine@gmail.com>
2026-07-27 21:29:37 -0400

    Configure protected Course 07 slug

---
+++ traumatologia/config.js
@@ -0,0 +1,7 @@
+window.KINECHECK_CONFIG = Object.freeze({
+  supabaseUrl: "https://eqhcdclyeoapmqtlduwf.supabase.co",
+  supabaseAnonKey: "sb_publishable_FTwhDZYCF3zf7W9rB7bFwQ_rF9Y7OX_",
+  courseSlug: "traumatologia-ortopedia-clinica",
+  courseKeyFunction: "course-key",
+  supportEmail: "emmanuelkine@gmail.com",
+});
