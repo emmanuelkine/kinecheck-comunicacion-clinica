8170c51514d434bee6051015f1fa8fe21dbcb0e0
emmanuelkine <emmanuelkine@gmail.com>
2026-08-05 16:53:28 -0400

    clinico: configure complementary guide access

---
+++ kinecheck-clinico-guia/config.js
@@ -0,0 +1,8 @@
+window.KINECHECK_GUIDE_CONFIG = Object.freeze({
+  supabaseUrl: "https://eqhcdclyeoapmqtlduwf.supabase.co",
+  supabaseAnonKey: "sb_publishable_FTwhDZYCF3zf7W9rB7bFwQ_rF9Y7OX_",
+  courseKeyFunction: "course-key",
+  accessSlug: "kinecheck-clinico",
+  supportUrl: "../soporte/",
+  academyUrl: "../academy/",
+});
