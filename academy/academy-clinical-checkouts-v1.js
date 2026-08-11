7dd832d4e3c570dd48e809d22774e864fce297e9
emmanuelkine <emmanuelkine@gmail.com>
2026-08-10 21:13:15 -0400

    Conecta checkout Hotmart de Pruebas Especiales

--- academy/academy-clinical-checkouts-v1.js
+++ academy/academy-clinical-checkouts-v1.js
@@ -6,5 +6,6 @@
   window.KINECHECK_CLINICAL_CHECKOUTS = Object.freeze({
     ...existing,
     "kinecheck-escalas": "https://pay.hotmart.com/G107106119P",
+    "kinecheck-pruebas-especiales": "https://pay.hotmart.com/N107106742S",
   });
 })();
